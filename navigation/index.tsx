b725cce4e43ee1fed611628216722cd4bf9276e6
Pavo <pzornija1@etf.unsa.ba>
2021-12-11 19:07:38 +0100

    Tab three added

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,7 +66,7 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+          title: 'Introduction',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
@@ -88,7 +88,15 @@ function BottomTabNavigator() {
         name="TabTwo"
         component={TabTwoScreen}
         options={{
-          title: 'Tab Two',
+          title: 'Languages',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="TabThree"
+        component={TabTwoScreen}
+        options={{
+          title: 'Contact us',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
